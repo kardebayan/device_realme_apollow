a2d4354c0c4a921c508ef557664f2aa2616c56eb
Debayan Kar <kardebayan3@gmail.com>
2022-06-28 18:25:58 +0530

    apollow: Import WiFi configs from stock
    * From realme/RMX3381/RE58A0L1:12/SP1A.210812.016/R.202204190946:user/release-keys

--- device.mk
+++ device.mk
@@ -36,3 +36,9 @@ PRODUCT_PACKAGES += \
     fstab.enableswap \
     fstab.mt6833 \
     ueventd.mt6833.rc
+
+# WiFi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
